343c346abec347d5a95a6693616072afc005afc4
matteo <mcombi@redhat.com>
2022-07-14 11:21:39 +0200

    some fix and new layout

--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@
 oc login -u opentlc-mgr -p r3dh4t1! $1
 oc adm policy add-cluster-role-to-user cluster-admin -z openshift-gitops-argocd-application-controller -n openshift-gitops
 argoPass=$(oc get secret/openshift-gitops-cluster -n openshift-gitops -o jsonpath='{.data.admin\.password}' | base64 -d)
-echo $argoPass
+echo "$argopass"
+echo "$argopass" > argopass.txt
 argoURL=$(oc get route openshift-gitops-server -n openshift-gitops -o jsonpath='{.spec.host}{"\n"}')
 echo $argoURL
