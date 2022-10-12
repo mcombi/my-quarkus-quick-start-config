c80ae30f0a4ad83d88f69550701b92df5db02ade
Matteo <mcombi@redhat.com>
2022-10-12 16:40:45 +0200

    removed user
    Signed-off-by: Matteo <mcombi@redhat.com>

--- enable-cluster-monitoring.sh
+++ enable-cluster-monitoring.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-oc login -u opentlc-mgr $1
+oc login $1
 oc apply -f monitoring/101-cluster-monitoring-config-map.yaml
 oc apply -f monitoring/102-workload-monitoring-config.yaml
 oc -n openshift-user-workload-monitoring get pod -w
